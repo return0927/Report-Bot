0ec3421bf1b5461ada1e4d5beaa906b763ee5db6
return0927 <bjleh0927@gmail.com>
2017-05-15 14:31:15 +0900

    Bot Optimization

--- kkutu_websocket.py
+++ kkutu_websocket.py
@@ -6,20 +6,13 @@ import json
 with codecs.open("setting.json","r",encoding="UTF-8") as f:
     settings = json.loads("".join(f.readlines()).replace("\r","").replace("\n",""))
 
-
+keys= [ str(x).split(":")[0].split("{")[1][1:-1] for x in settings['numbers']]
+numbers= [ str(x).split(":")[1].split("}")[0][2:-1] for x in settings['numbers']]
 
 is_first = True
 
 time = strftime("-%Y%m%d-%H%M%S")
 
-"""
-root = Tk()
-lb = Label(root)
-lb.pack(anchor="w")
-lb.config(text="==============================", justify="left", font=('굴림체',10))
-
-root.update()
-"""
 
 def procedure(ws, message):
     command(ws,message)
@@ -27,52 +20,61 @@ def procedure(ws, message):
 def whisper(to, m):
     global ws
     ws.send('{"type":"talk","whisper":"'+to+'","value":"'+m+'"}')
+
+def message(m):
+    global ws
+    ws.send('{"type":"talk","value":"'+m+'"}')
+
+
+    
 def command(ws,message):
     global is_first
     if is_first == True:
         with codecs.open("output"+time+".txt","w",encoding="UTF-8") as f:
             f.write(" #### Daemon Started ####\n")
         is_first = False
-        #ws.send('{"type":"talk","value":""}')
-        #ws.send('{"type":"talk","value":"신고 자동화시스템 제작중입니다. 제주도여행중에 일하고있습니다 ㅠ"}')
-        #ws.send('{"type":"talk","value":"Powered by [운영자]이은학"}')
+        
         ws.send('{"type":"talk","whisper":"이은학","value":"=== Report Daemon Started ==="}')
-        #print(" ## Socket Message %d length" % len(message))
         pass
     else:
-        #with codecs.open("output"+time+".txt","a",encoding="UTF-8") as f:
-        #    f.write(message+"\n")
-        #print(" ## Socket Message %d length" % len(message))
         jsonstring = json.loads(message)
         if "from" in jsonstring.keys():
             print(" Whisper from %s : %s"% (jsonstring['from'],jsonstring['value']))
             cmd = jsonstring['value'].split(" ")
+
+            
             if cmd[0] == "!commands":
                 whisper(jsonstring['from']," 안녕하세요 끄투 신고봇입니다. 아래는 명령어 목록입니다.")
-                whisper(jsonstring['from']," !report [신고번호] [고유번호 or 닉네임] [자세한 설명] / 유저를 신고하는 기능입니다.")
+                whisper(jsonstring['from']," !report [신고번호] [고유번호 or 닉네임(띄어쓰기없음)] [자세한설명] / 유저를 신고하는 기능입니다.")
                 whisper(jsonstring['from']," !number / 신고번호를 조회합니다.")
                 whisper(jsonstring['from']," !info / 끄투봇의 정보를 봅니다.")
-                #whisper(jsonstring['from']," !")
+                
             elif cmd[0] == "!report":
-                whisper(jsonstring['from'], " 신고가 접수되었습니다. 유저정보와 시각이 모두 기록되므로 허위신고는 처벌받을 수 있습니다.")
-                print(" %d" %len(cmd))
-                print(" %s " %cmd[1])
-                print(" %s " %" ".join(cmd[2:]))
+                arg={};
+                if cmd < 4:
+                    whisper(jsonstring['from'], " 잘못된 명령어 사용입니다. ")
+                    whisper(jsonstring['from'], " !report [신고번호] [고유번호 or 닉네임(띄어쓰기없음)] [자세한설명]")
+                else:
+                    arg['num']=cmd[1]; arg['person']=cmd[2:]; arg['reason'] = cmd[3:]
+                    
+                
+                
             elif cmd[0] == "!number":
-                whisper(jsonstring['from'], " K001 누가 저에게 비속어와 비방하는 말을 해서 기분이 나빠요!")
-                whisper(jsonstring['from'], " K002 채팅창에서 비슷한 문구를 계속 작성해서 보기 안좋아요!")
-                whisper(jsonstring['from'], " K003 여기 방 제목이 이상해요!")
-                whisper(jsonstring['from'], " K004 게임이 이상해요! ")
-                whisper(jsonstring['from'], " K005 뭐가 이상한지는 모르겠는데 이상해요!")
+                for n in range(len(keys)):
+                    whisper(jsonstring['from'], keys[n]+" "+numbers[n])
                 whisper(jsonstring['from'], " -- 위의 코드는 신고시에 필요한 코드입니다! 해당하는것을 골라주세요. --")
+                
             elif cmd[0] == "!info":
                 whisper(jsonstring['from']," KKuTu Korea - Report Bot.")
                 whisper(jsonstring['from']," Powered By. 이은학(Eunhak Lee)")
                 whisper(jsonstring['from']," https://github.com/return0927")
             else:
                 whisper(jsonstring['from']," 잘못된 명령어입니다. !commands 를 입력해서 명령어를 확인해보세요!")
-                
-        #lb.config(text=message, justify="left", font=('굴림체',10))
+        elif jsonstring['type'] == "chat":
+            if jsonstring['value'] == "신고봇나와라":
+                ws.send('{"type":"talk","value":"안녕하세요 신고봇입니다! 명령어 사용은 귓속말로 해주세요 :D (귓속말은 /ㄷ [닉네임] [할말] 로 하실 수 있습니다!)"}')
+            elif jsonstring['value'] == '앙':
+                ws.send('{"type":"talk","value":"기모띠"}')
 
 def on_close(ws):
     print(" ## Socket died ")
